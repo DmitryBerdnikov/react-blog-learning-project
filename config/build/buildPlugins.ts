3b464ac6e105eb6699d56a0244376ee3630d8d09
Dmitry Berdnikov <berdnikovdmitry@mail.ru>
2023-01-30 09:28:25 +0500

    add webpack config with devServer, typescript, react, sass

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,12 @@
+import { WebpackPluginInstance, ProgressPlugin } from 'webpack';
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+import { BuildOptions } from "./types/config";
+
+export function buildPlugins({ paths }: BuildOptions): WebpackPluginInstance[] {
+    return [
+        new HTMLWebpackPlugin({
+            template: paths.html,
+        }),
+        new ProgressPlugin(),
+    ]
+}
